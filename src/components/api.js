53f93ef04efaabfa350943cb78527173af8c5bad
Julia Wang <jlwang98@gmail.com>
2025-01-10 04:04:40 -0800

    encoded password-driven code samples

--- src/components/api.js
+++ src/components/api.js
@@ -1,11 +1,16 @@
-import fs from 'fs';
 import { Octokit } from '@octokit/core';
+import { Buffer } from 'buffer';
 
-const { token, owner, repo, key } = JSON.parse(fs.readFileSync('../../config.json', 'utf8'));
+const token = process.env.REACT_APP_TOKEN;
+const owner = process.env.REACT_APP_OWNER;
+const repo = process.env.REACT_APP_REPO;
+const filePath = process.env.REACT_APP_FILE_PATH;
+
 const octokit = new Octokit({
     auth: token
 })
-const lockedFiles = JSON.parse(await getFileContent(key));
+
+const fileContent = await getFileContent(filePath);
 
 async function getFileContent(path) {
     try {
@@ -18,25 +23,26 @@ async function getFileContent(path) {
                 "X-GitHub-Api-Version": "2022-11-28"
             }
         });
-        // Decode file content
-        const content = Buffer.from(response.data.content, "base64").toString("utf-8");
-        return content;
+        // Return file content
+        return response.data.content;
     } 
     catch (error) {
         console.error("[ERROR] Fetching file:", error.message);
-        return "";
+        return "{}";
     }
 }
 
 export async function submitPassword(password) {
     // Get files
-    const files = lockedFiles[password];
+    const files = JSON.parse(Buffer.from(fileContent, "base64").toString("utf-8"))[password];
     // Return contents of all the files
-    const contents = [];
+    const passwordContents = [];
     if (files !== undefined) {
         for (let i = 0; i < files.length; i++) {
-            contents[i] = await getFileContent(files[i]);
+          // Decode content
+          const content = await getFileContent(files[i]);
+          passwordContents[i] = Buffer.from(content, "base64").toString("utf-8");
         }
     }
-    return contents;
+    return passwordContents;
 }
