9231b1b0254519fdfbc6fdf17544cc10f9b8fac6
Julia Wang <jlwang98@gmail.com>
2025-01-11 03:12:20 -0800

    Update about me content

--- src/components/about-me.js
+++ src/components/about-me.js
@@ -21,34 +21,33 @@ const AboutMe = (props) => {
             <Fragment>
               <p className="thq-body-large thq-section-max-width">
                 <span>
-                  Hi there! I’m <b>Julia</b>, a software engineer and game developer currently pursuing
-                  a B.S. in <b>Computer Science</b> with a minor in <b>Immersive Media</b> at 
-                  the University of Southern California.
+                  Hi there! I’m <b>Julia</b>, a fourth-year <b>Computer Science</b> student pursuing a B.S. and M.S. and
+                  minor in <b>Immersive Media</b> at the University of Southern California.
                 </span>
                 <br></br>
                 <br></br>
                 <span>
-                  As a <b>software engineer</b>, I’m motivated to apply my programming and problem-solving
-                  skills to build tools and platforms that <b>tackle real-world challenges</b>, foster connection, 
-                  and <b>positively impact</b> people’s everyday lives. I thrive in environments driven 
-                  by <b>passion</b> and <b>collaboration</b> and am always excited when faced with a new problem 
-                  to solve!
+                  As an <b>engineer</b>, I’m motivated to apply my programming and problem-solving
+                  skills to build tools and platforms that tackle real-world challenges, foster connection, 
+                  and positively impact people’s everyday lives. I thrive in environments driven 
+                  by <b>passion and collaboration</b> and am always excited when faced with new problems 
+                  to tackle!
                 </span>
                 <br></br>
                 <br></br>
                 <span>
-                  As a <b>game developer</b>, I’m passionate about developing <b>interactive and immersive experiences</b> that 
-                  tell stories and evoke emotions that can <b>bring people together</b>, deepening our understanding of ourselves 
-                  and one another. I am always eager to <b>explore </b> innovative ways to use technology and push the boundaries of
-                  storytelling — whether it’s through experimenting with alternative controllers, virtual reality, or other
-                  cutting-edge mediums.
+                  As a <b>game developer</b> and <b>artist</b>, I’m passionate about developing interactive and immersive 
+                  experiences that are not only <b>fun</b> but also evoke emotions capable of connecting and <b>bringing 
+                  people together</b>! I am always eager to explore new and innovative ways to use technology and <b>push the 
+                  boundaries</b> of media — whether it’s through experimenting with alternative controllers, virtual reality, 
+                  or other cutting-edge mediums.
                 </span>
                 <br></br>
                 <br></br>
                 <span>
                   When I’m not working, you can find me listening and singing
                   along to music, supporting local artists at conventions, or
-                  playing games (of course!).
+                  playing games!
                 </span>
                 <br></br>
               </p>
